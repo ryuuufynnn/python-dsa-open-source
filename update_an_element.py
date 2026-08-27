469dd8c315ceb44e94f01cdd58973923875a822b
lau-linux <laurence060723@gmail.com>
2026-08-27 11:53:32 +0800

    update_an_element.py convert into function

--- update_an_element.py
+++ update_an_element.py
@@ -1,10 +1,10 @@
-user_input = input("Enter number with spaces: ")
+# user_input = input("Enter number with spaces: ")
 
-numbers = [int(x) for x in user_input.split()]
+# numbers = [int(x) for x in user_input.split()]
 
-target = int(input("Enter a target number: "))
+# target = int(input("Enter a target number: "))
 
-new_value = int(input("Enter a new value: "))
+# new_value = int(input("Enter a new value: "))
 
 # for num in numbers:
 #     if num == target:
@@ -14,9 +14,21 @@ new_value = int(input("Enter a new value: "))
 # print(numbers)
 
 # approach 2 what would you choose prof?
-for num in range(len(numbers)):
-    if numbers[num] == target:
-        numbers[num] = new_value
-        break
+# for num in numbers:
+#     if numbers[num] == target:
+#         numbers[num] = new_value
+#         break
 
-print(numbers)
+# print(numbers)
+
+def update_element(nums: list[int], target: int, new_value: int) -> int:
+    for num in nums:
+        if nums[num] == target:
+            nums[num] = new_value
+            break # if isa lang ang number na gustong i update:
+            # input: 1 2 3 2 2
+            # target: 2
+            # output: 1 4 3 2 2
+    return nums
+
+print(update_element([1, 2, 3, 2, 2], 2, 4))
