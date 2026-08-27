49c13357e07d0c724fcd0b4743b3481b8618a548
lau-linux <laurence060723@gmail.com>
2026-08-27 16:56:28 +0800

    build_a_new_list.py converted into function

--- build_a_new_list.py
+++ build_a_new_list.py
@@ -1,11 +1,24 @@
-user_input = input("Enter numbers with spaces: ")
+# user_input = input("Enter numbers with spaces: ")
 
-numbers = [int(x) for x in user_input.split()]
+# numbers = [int(x) for x in user_input.split()]
 
-greater_numbers = []
+# greater_numbers = []
 
-for num in numbers:
-    if num > 5:
-        greater_numbers.append(num)
+# for num in numbers:
+#     if num > 5:
+#         greater_numbers.append(num)
 
-print(f"Numbers greater than 5: {greater_numbers}")
+# print(f"Numbers greater than 5: {greater_numbers}")
+
+
+def build_a_new_list(nums: list[int], greater_than: int) -> int:
+    greater_numbers = []
+
+    for num in nums:
+        if num > greater_than:
+            greater_numbers += [num]
+    return greater_numbers
+
+nums = [1, 2, 5, 9, 8, 7]
+five = 5
+print(build_a_new_list(nums, five))
