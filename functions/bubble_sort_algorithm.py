1bbbc1f35fbb98afa2a0353c0c9839c2f929cb95
lau-linux <laurence060723@gmail.com>
2026-08-28 10:23:33 +0800

    function for bubble sort

--- functions/bubble_sort_algorithm.py
+++ functions/bubble_sort_algorithm.py
@@ -1,6 +1,6 @@
-# from [5, 4, 3, 2, 1] to [1, 2, 3, 4, 5]
+# from [5, 4, 3, 2, 1] to [1, 2, 3, 4, 5] - aayusin ang list of numbers in ascending order;
 
-def bubble_sort(array):
+def bubble_sort(array: list[int]) -> list[int]:
     is_swap = True
     right = len(array) -1
 
@@ -13,4 +13,4 @@ def bubble_sort(array):
                 array[i] = temp
                 is_swap = True
     return array
-print(bubble_sort([4,8,10,1])) 
+print(bubble_sort([4,8,10,1]))
